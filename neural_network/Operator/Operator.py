d2fbdb903aa97d41183e8946bc6157133b2b2e92
inory.yang <inory.yang@extremevision.com.cn>
2017-03-28 20:25:33 +0800

    改变Operator里的数据结构，全部用numpy.ndarray传递，出入接口可以是float或list

--- neural_network/Operator/Operator.py
+++ neural_network/Operator/Operator.py
@@ -14,16 +14,17 @@ class Operator(object):
 
     # Dict to store relation between forward function and operation.Add new operations here
     symbol_dict = {
-                    '+':'add', 
+                    '+':'add',
+                    '-':'sub',
                     '*':'mul',
-                    'exp':'exp'
+                    'exp':'exp',
+                    'pow':'pow'
                   }
     
     def __init__(self, operation, input=None):
         ''' private '''
         # Variables
         self._operation = operation
-        self._input = input
         self._input_size = 0
         self._residual = None
 
@@ -33,10 +34,12 @@ class Operator(object):
 
         ''' public '''
         # Variables
+        self.forward_input = input
+        self.backward_input = None
         self.forward_output = None
         self.backward_output = None
-        self.forward_funcs = (self.forward_add, self.forward_mul, self.forward_exp)
-        self.backward_funcs = (self.backward_add, self.backward_mul, self.backward_exp)
+        self.forward_funcs = (self.forward_add, self.forward_sub, self.forward_mul, self.forward_exp, self.forward_pow)
+        self.backward_funcs = (self.backward_add, self.backward_sub, self.backward_mul, self.backward_exp, self.backward_pow)
 
         # Init
         self._parse(operation)
@@ -49,41 +52,30 @@ class Operator(object):
     def input_size(self):
         return self._input_size
 
-    @property
-    def input(self):
-        return self._input
-
-    @property
-    def residual(self):
-        return self._residual
-
-    def set_input(self, _input):
-        self._input_size = len(_input)
-        if self._input_size > 2:
-            raise ValueError('The number of operands must be less than or equal to 2')
-        self._input = _input
-
-    def set_residual(self, _residual):
-        self._residual = _residual
-
     def forward(self, _input=None):
         if _input == None:
-            _input = self.input
+            _input = self.forward_input
+        if isinstance(_input, list):
+            for i, x in enumerate(_input):
+                if isinstance(x, Operator):
+                    _input[i] = x.forward_output
         else:
-            self._input_size = len(_input)
-            if self._input_size > 2:
-                raise ValueError('The number of operands must be less than or equal to 2')
-
+             _input = [_input.forward_output] if isinstance(_input, Operator) else [_input]
+        _input = np.array(_input)
         self.forward_output = self._forward_func(_input)
         return self.forward_output
 
     def backward(self, _residual=None):
         if _residual == None:
-            _residual = self.residual
-
+            _residual = self.backward_input
+        if isinstance(_residual, list):
+            for i, x in enumerate(_residual):
+                if isinstance(x, Operator):
+                    _residual[i] = x.backward_output
+        else:
+             _residual = [_residual.backward_output] if isinstance(_residual, Operator) else [_residual]
+        _residual = np.array(_residual)
         self.backward_output = self._backward_func(_residual)
-        if isinstance(self.input[0], Operator):
-            self.input[0].set_residual(self.backward_output)
         return self.backward_output
 
     ''' Register operation functions here '''
@@ -105,36 +97,38 @@ class Operator(object):
         Define all kinds of operations.Both forward and backward .
         Function names must be a pair of forward_xxx and backward_xxx
     '''
-    def _forward_check_params(self, params):
-        if not isinstance(params, list):
-            params = [params]
-        import copy
-        tmp_params = copy.deepcopy(params)
-        for i, param in enumerate(params):
-            if isinstance(param, Operator):
-                tmp_params[i] = param.forward_output
-        return tmp_params
-
     # add
     def forward_add(self, params):
-        tmp_params = self._forward_check_params(params)
-        return [reduce(lambda x,y:x+y, tmp_params)]
+        return params[0]+params[1]
 
     def backward_add(self, param):
         return param
 
     # mul
     def forward_mul(self, params):
-        tmp_params = self._forward_check_params(params)
-        return [reduce(lambda x,y:x*y, tmp_params)]
+        return params[0]*params[1]
 
     def backward_mul(self, param):
         return param*self.input[1]
 
     # exp
-    def forward_exp(self, params):
-        tmp_params = self._forward_check_params(params)
-        return np.exp(tmp_params).tolist()
+    def forward_exp(self, param):
+        return np.exp(param)
 
     def backward_exp(self, param):
-        return param*np.exp(self.input[1]) 
+        return param*self.forward_output
+    
+    # sub
+    def forward_sub(self, params):
+        return params[0]-params[1]
+
+    def backward_sub(self, param):
+        return param
+
+    # pow
+    def forward_pow(self, params):
+        return np.power(params[0], params[1])
+
+    def backward_pow(self, param):
+        x, power = self.input
+        return param*power*np.power(x, power-1) 
--- neural_network/test.py
+++ neural_network/test.py
@@ -1,12 +1,14 @@
 from Operator import *
+import sys
 
 def forward(x):
     op_mul1 = Operator('*', [x, -1])
-    op_exp1 = Operator('exp', [op_mul1])
+    op_exp1 = Operator('exp', op_mul1)
     op_add1 = Operator('+', [op_exp1, 1])
-    op_mul1.forward()
-    op_exp1.forward()        
-    result = op_add1.forward()
-    return result
+    op_pow1 = Operator('pow', [op_add1, -1])
+    print op_mul1.forward()
+    print op_exp1.forward()        
+    print op_add1.forward()
+    print op_pow1.forward()
 
-print forward(0)
+forward(float(sys.argv[1]))
