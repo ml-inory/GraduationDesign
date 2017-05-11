c5234b807b8d36961cb3dc1d067bf9922db589b0
ml-inory <rzyang@yangrongzhaodeMacBook-Pro.local>
2017-05-12 03:22:27 +0800

    删掉废物

---
+++ ev_face/crop_face/build/CMakeFiles/crop_face.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crop_face.dir/main.cpp.o"
+  "crop_face.pdb"
+  "crop_face"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crop_face.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
